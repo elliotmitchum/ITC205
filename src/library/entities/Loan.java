653353ce6d640c7f1005bac1d1b1b37aee97be35
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-07-26 07:36:59 +1000

    Fix spacing

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -49,13 +49,13 @@ public class Loan implements Serializable {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         StringBuilder sb = new StringBuilder();
         sb.append("Loan:  ").append(loanId).append("\n")
-                .append("  Borrower ").append(patron.GeT_ID()).append(" : ")
-                .append(patron.GeT_FiRsT_NaMe()).append(" ").append(patron.GeT_LaSt_NaMe()).append("\n")
-                .append("  Item ").append(item.GeTiD()).append(" : ")
-                .append(item.GeTtYpE()).append("\n")
-                .append(item.GeTtItLe()).append("\n")
-                .append("  DueDate: ").append(sdf.format(dueDate)).append("\n")
-                .append("  State: ").append(state);
+            .append("  Borrower ").append(patron.GeT_ID()).append(" : ")
+            .append(patron.GeT_FiRsT_NaMe()).append(" ").append(patron.GeT_LaSt_NaMe()).append("\n")
+            .append("  Item ").append(item.GeTiD()).append(" : ")
+            .append(item.GeTtYpE()).append("\n")
+            .append(item.GeTtItLe()).append("\n")
+            .append("  DueDate: ").append(sdf.format(dueDate)).append("\n")
+            .append("  State: ").append(state);
         return sb.toString();
     }
 
