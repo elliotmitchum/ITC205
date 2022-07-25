c7d655a43f16d4e39e51a5dc7b66705f0c60fc2b
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-07-26 07:24:11 +1000

    Uppercase constants

--- src/library/entities/ItemType.java
+++ src/library/entities/ItemType.java
@@ -7,20 +7,20 @@ public enum ItemType {
 
     Book, Dvd, Vhs, Cd, Cassette;
 
-    private static final ItemType[] values = values();
+    private static final ItemType[] VALUES = values();
 
-    private static final Map<ItemType, String> itemTypes = new HashMap<ItemType, String>();
+    private static final Map<ItemType, String> ITEM_TYPES = new HashMap<ItemType, String>();
 
     static {
-        itemTypes.put(ItemType.Book, "Book");
-        itemTypes.put(ItemType.Dvd, "DVD");
-        itemTypes.put(ItemType.Vhs, "VHS Video cassette");
-        itemTypes.put(ItemType.Cd, "CD Audio disk");
-        itemTypes.put(ItemType.Cassette, "Audio cassette");
+        ITEM_TYPES.put(ItemType.Book, "Book");
+        ITEM_TYPES.put(ItemType.Dvd, "DVD");
+        ITEM_TYPES.put(ItemType.Vhs, "VHS Video cassette");
+        ITEM_TYPES.put(ItemType.Cd, "CD Audio disk");
+        ITEM_TYPES.put(ItemType.Cassette, "Audio cassette");
     }
 
     public String toString() {
-        return itemTypes.get(this);
+        return ITEM_TYPES.get(this);
     }
 
 }
