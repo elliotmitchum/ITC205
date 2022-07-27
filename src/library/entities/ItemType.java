fbc47bf037ece96fd5aaeccffa36597dd973f388
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-07-28 07:36:41 +1000

    Fix whitespacing

--- src/library/entities/ItemType.java
+++ src/library/entities/ItemType.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ItemType {
-    BOOK, DVD, VHS, CD, CASSETTE;
     
+    BOOK, DVD, VHS, CD, CASSETTE;
+
     private static final ItemType[] VALUES = values();
 
     private static final Map<ItemType, String> VALUE_REPR_MAP = new HashMap<ItemType, String>();
+
     static {
         VALUE_REPR_MAP.put(ItemType.BOOK, "Book");
         VALUE_REPR_MAP.put(ItemType.DVD, "DVD");
@@ -16,8 +18,8 @@ public enum ItemType {
         VALUE_REPR_MAP.put(ItemType.CD, "CD Audio disk");
         VALUE_REPR_MAP.put(ItemType.CASSETTE, "Audio cassette");
     }
-    
-    public  String toString() {
+
+    public String toString() {
         return VALUE_REPR_MAP.get(this);
     }
 
