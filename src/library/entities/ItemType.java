4283b6d5b3a7d5a9b4ed8eca90368bc7ff0b84c8
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-07-28 07:40:10 +1000

    Rename VALUE_REPR_MAP to LABELS

--- src/library/entities/ItemType.java
+++ src/library/entities/ItemType.java
@@ -4,23 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ItemType {
-    
+
     BOOK, DVD, VHS, CD, CASSETTE;
 
     private static final ItemType[] VALUES = values();
 
-    private static final Map<ItemType, String> VALUE_REPR_MAP = new HashMap<ItemType, String>();
+    private static final Map<ItemType, String> LABELS = new HashMap<ItemType, String>();
 
     static {
-        VALUE_REPR_MAP.put(ItemType.BOOK, "Book");
-        VALUE_REPR_MAP.put(ItemType.DVD, "DVD");
-        VALUE_REPR_MAP.put(ItemType.VHS, "VHS Video cassette");
-        VALUE_REPR_MAP.put(ItemType.CD, "CD Audio disk");
-        VALUE_REPR_MAP.put(ItemType.CASSETTE, "Audio cassette");
+        LABELS.put(ItemType.BOOK, "Book");
+        LABELS.put(ItemType.DVD, "DVD");
+        LABELS.put(ItemType.VHS, "VHS Video cassette");
+        LABELS.put(ItemType.CD, "CD Audio disk");
+        LABELS.put(ItemType.CASSETTE, "Audio cassette");
     }
 
     public String toString() {
-        return VALUE_REPR_MAP.get(this);
+        return LABELS.get(this);
     }
 
 }
