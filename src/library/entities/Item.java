a01ed13570cf6fae1daf979052ba0bbf1a3fc041
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-07-28 07:41:54 +1000

    Fix whitespacing

--- src/library/entities/Item.java
+++ src/library/entities/Item.java
@@ -1,20 +1,22 @@
 package library.entities;
-import java.io.Serializable;
 
+import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Item implements Serializable {
-    
+
     private ItemType TyPe;
+
     private String aUtHoR;
+
     private String TiTlE;
+
     private String CaLlNo;
+
     private long Id;
-    
-    private enum iTeM_StAtE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
+
     private iTeM_StAtE sTaTe;
-    
-    
+
     public Item(String AuThOr, String tItLe, String cAlLnO, ItemType ItEmTyPe, long iD) {
         this.TyPe = ItEmTyPe;
         this.aUtHoR = AuThOr;
@@ -23,16 +25,15 @@ public class Item implements Serializable {
         this.Id = iD;
         this.sTaTe = iTeM_StAtE.AVAILABLE;
     }
-    
+
     public String toString() {
         StringBuilder Sb = new StringBuilder();
         Sb.append("Item: ").append(Id).append("\n")
-          .append("  Type:   ").append(TyPe).append("\n")
-          .append("  Title:  ").append(TiTlE).append("\n")
-          .append("  Author: ").append(aUtHoR).append("\n")
-          .append("  CallNo: ").append(CaLlNo).append("\n")
-          .append("  State:  ").append(sTaTe);
-        
+            .append("  Type:   ").append(TyPe).append("\n")
+            .append("  Title:  ").append(TiTlE).append("\n")
+            .append("  Author: ").append(aUtHoR).append("\n")
+            .append("  CallNo: ").append(CaLlNo).append("\n")
+            .append("  State:  ").append(sTaTe);
         return Sb.toString();
     }
 
@@ -48,57 +49,51 @@ public class Item implements Serializable {
         return TyPe;
     }
 
-
-    
     public boolean Is_AvAiLaBlE() {
         return sTaTe == iTeM_StAtE.AVAILABLE;
     }
 
-    
     public boolean Is_On_LoAn() {
         return sTaTe == iTeM_StAtE.ON_LOAN;
     }
 
-    
     public boolean Is_DaMaGeD() {
         return sTaTe == iTeM_StAtE.DAMAGED;
     }
 
-    
     public void TaKeOuT() {
-        if (sTaTe.equals(iTeM_StAtE.AVAILABLE)) 
+        if (sTaTe.equals(iTeM_StAtE.AVAILABLE))
             sTaTe = iTeM_StAtE.ON_LOAN;
-        
-        else 
-            throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", sTaTe));
-        
-        
-    }
 
+        else
+            throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", sTaTe));
+
+
+    }
 
     public void TaKeBaCk(boolean DaMaGeD) {
-        if (sTaTe.equals(iTeM_StAtE.ON_LOAN)) 
-            if (DaMaGeD) 
-                sTaTe = iTeM_StAtE.DAMAGED;            
+        if (sTaTe.equals(iTeM_StAtE.ON_LOAN))
+            if (DaMaGeD)
+                sTaTe = iTeM_StAtE.DAMAGED;
 
-            else 
-                sTaTe = iTeM_StAtE.AVAILABLE;        
+            else
+                sTaTe = iTeM_StAtE.AVAILABLE;
 
-        
-        else 
+
+        else
             throw new RuntimeException(String.format("Item: cannot return item while item is in state: %s", sTaTe));
-                
+
     }
 
-    
     public void rEpAiR() {
-        if (sTaTe.equals(iTeM_StAtE.DAMAGED)) 
+        if (sTaTe.equals(iTeM_StAtE.DAMAGED))
             sTaTe = iTeM_StAtE.AVAILABLE;
-        
-        else 
+
+        else
             throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", sTaTe));
-        
+
     }
 
+    private enum iTeM_StAtE {AVAILABLE, ON_LOAN, DAMAGED, RESERVED}
 
 }
